a9b00d885f094910a80212898ae1305ef4a583c4
danteuri <santiagomaytahuanca@gmail.com>
2023-12-16 22:44:17 -0300

    1 day
    viendo un video de curso de poo con php y otro con el link de 3 horas de la profe jennifer lujan orellana
    el video de la profe estaba bueno hasta ahora, no hubieron problemas y me resolvio cosas de boostrap. gracias profe :D/

---
+++ poophp/mono.php
@@ -0,0 +1,7 @@
+<?php
+
+class mono{
+    public $monito;
+}
+
+?>
---
+++ sitiowebphp/template/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Politicos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Partidos</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
+        <div class="row">
